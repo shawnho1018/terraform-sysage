d7eae7f5c85a0e647700e71cbdd88367ae19f987
shawnho1018 <hos@vmware.com>
2020-01-02 01:05:04 +0800

    change VM's topology

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "virtual-machine" {
   vs_cluster_name            = "syspks"
   vs_vm_folder               = "ClusterAPI"
   vs_rp_name                 = "VCS"
-  vs_network_name            = "PG-MGMT"
+  vs_network_name            = "clusterapi"
   vm_template_name           = "ubuntu1804"
   
   dns_servers                = ["10.9.25.31"]
@@ -22,7 +22,28 @@ module "virtual-machine" {
   vm_cpus                    = "2"
   vm_mem                     = "2048"
   vm_disk_size               = "60"
-  vms                        = {"vm-1" = "10.9.25.70"}
-  ip_netmask                 = "25"
-  ip_gateway                 = "10.9.25.126"
+  vms                        = {"client" = "192.168.0.101"}
+  ip_netmask                 = "24"
+  ip_gateway                 = "192.168.0.1"
+}
+module "virtual-machine1" {
+  source  = "ulm0/vms/vsphere"
+  version = "0.12.0"
+  # Parameters for vSphere submodule
+  vs_dc_name                 = "SDDC-SYS"
+  vs_ds_name                 = "vsanDatastore"
+  vs_cluster_name            = "syspks"
+  vs_vm_folder               = "ClusterAPI"
+  vs_rp_name                 = "VCS"
+  vs_network_name            = "LS1"
+  vm_template_name           = "ubuntu1804"
+  
+  dns_servers                = ["10.9.25.31"]
+  domain                     = "syspks.com"
+  vm_cpus                    = "2"
+  vm_mem                     = "2048"
+  vm_disk_size               = "60"
+  vms                        = {"server" = "192.168.2.2"}
+  ip_netmask                 = "24"
+  ip_gateway                 = "192.168.2.1"
 }
